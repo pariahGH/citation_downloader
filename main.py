66ff663898f72729c1b0fa1e7263352ff95b9325
pariah <pariah@teksavvy.tech>
2018-12-29 23:05:43 -0600

    Refactor for readability
    Break up the downloading functionality into discrete functions
    Move downloading related functionality into util.py
    Create logger class, encapsulate file writing and log display
    Create SciHubError type, throw exception issue with Sci-Hub captchas
    Detect retries via SciHubError exception
    Automatically add newline when logging to display
    Add friendly reminder when an email address is not provided
    Remove unused imports re: captcha solving

--- main.py
+++ main.py
@@ -1,120 +1,59 @@
-from bs4 import BeautifulSoup
-import requests
-import urllib.parse
-import sys
 import os
 import time
 import wx
 from threading import Thread
-import pytesseract
-from PIL import Image
 import json
+import util
 
-# read in the file, split on "\n\n" - all citation blocks must be separated by two consecutive newlines (one blank space between them)
 class PDFThread(Thread):
-	def __init__(self, filename, mailName, logDisplay, saveDir, startButton):
+	def __init__(self,options, logger, startButton):
 		Thread.__init__(self)
-		self.filename = filename
-		self.mailName = mailName
-		self.logDisplay = logDisplay
-		self.saveDir = saveDir
+		self.fileName = options["fileName"]
+		self.mailName = options["mailName"]
+		self.saveDir = options["saveDir"]
+		self.logger = logger
 		self.startButton = startButton
-	
-	def getFromSciHub(self, data,w):
-		try:
-			pdfUrl = ""
-			self.logDisplay.AppendText("Attempting to get PDF url...\n")
-			#get from scihub - initiate download, but first check if there was a captcha - if so print to stdout, wait 10 seconds, try again
-			articlePage = BeautifulSoup(requests.get("https://sci-hub.tw/"+data["doi"]).text,'html5lib')
-			iframes = articlePage.find_all("iframe")
-			if len(iframes) > 0:
-				pdfUrl = iframes[0]["src"]
-			else:
-				self.logDisplay.AppendText("Hit PDF URL captcha, adding to retry list\n\n")
-				return data
-					
-			self.logDisplay.AppendText("PDF url found for: " + data["title"]+"\n")
-			#check if the response is a captcha by checking content type
-			#this can also be tweaked to download the captcha
-			response = requests.get(pdfUrl)
-			if(response.headers["Content-Type"] != "application/pdf"):
-				self.logDisplay.AppendText("Hit PDF download captcha, adding to retry list\n\n")
-				return data
-				
-			with open(self.saveDir+"/"+urllib.parse.quote(data["title"][0:99],safe='')+".pdf",'wb') as dest:
-				dest.write(response.content)
-			# write to log 
-			w.write(data["citation"]+"\t"+data["doi"]+"\t"+data["title"]+"\n")
-			self.logDisplay.AppendText("Downloaded PDF!\n\n")
-			return ""
-		except Exception as e:
-			self.logDisplay.AppendText("Hit timeout, adding to retry list\n\n")
-			return data
 		
 	def run(self):
-		with open(self.filename,'r', encoding="utf8") as f:
-			#create our write file
-			if not os.path.exists(self.saveDir):
-				os.makedirs(self.saveDir)
-			
-			with open(self.saveDir+'/list.txt','w', encoding="utf8") as w:
-				data = f.read()
-				captchaArray = []
-				# read in every citation, make a query against crossref, extract the doi from the first result, and download it from scihub
-				for citationRaw in data.split("\n\n"):
-					citation = citationRaw.replace("\n"," ")
-					if(citation != ""):
-						self.logDisplay.AppendText("Looking up metadata...\n")
-						try:
-							data = requests.get("https://api.crossref.org/works?"+urllib.parse.urlencode({"query.bibliographic":citation,"mailto":self.mailName}))
-							response = data.json()["message"]['items'][0]
-								
-							doi = response["DOI"]
-							score = response["score"]
-							title = response["title"][0]
-								
-							self.logDisplay.AppendText("Found DOI!\n")
-							
-							#get the iframe that holds the pdf
-							returned = self.getFromSciHub({"doi":doi,"title":title,"citation":citation},w)
-							if(returned != ""):
-								captchaArray.append(returned)
-						except Exception as e:
-							print(data)
-							print(citation)
-						
-				# retry!
-				self.logDisplay.AppendText("Done! Checking retry list...\n\n")
-				length = len(captchaArray)
-				retries = 0
-				while(length >0 and retries != 5):
-					self.logDisplay.AppendText(str(length)+" retries found! Waiting 30 minutes...\n\n")
-					time.sleep(1800)
-					temp = captchaArray
-					captchaArray = []
-					for retry in temp:
-						returned = self.getFromSciHub(retry,w)
-						if(returned != ""):
-							captchaArray.append(returned)
-					retries += 1
-					length = len(captchaArray)
-				if(retries == 5):
-					self.logDisplay.AppendText("Max retries hit, writing leftovers to file!\n\n")
-					# iterate over the retries thing and write the citations
-					with open('./retries.txt','w', encoding="utf8") as retryDest:
-						for retry in captchaArray:
-							retryDest.write(retry["citation"]+"\n\n")
-				self.logDisplay.AppendText("Done!")
-				self.startButton.Enable()
+		with open(self.fileName,'r', encoding="utf8") as f:
+			data = f.read()
+			errorArray = []
+			for citation in data.split("\n\n"):
+				try:
+					util.getPDF(citation, self.logger, self.mailName)
+				except util.SciHubError as e:
+					errorArray.append(e.data)
+			# retry!
+			self.logger.logToDisplay("Done! Checking retry list...\n")
+			length = len(errorArray)
+			retries = 0
+			while(length >0 and retries != 5):
+				self.logger.logToDisplay(str(length)+" retries found! Waiting 30 minutes...\n")
+				time.sleep(1800)
+				temp = errorArray
+				errorArray = []
+				for citation in temp:
+					try:
+						util.getPDF(citation, self.logger, self.mailName)
+					except util.SciHubError as e:
+						errorArray.append(e.data)
+				retries += 1
+				length = len(errorArray)
+			if(retries == 5):
+				self.logger.logToDisplay("Max retries hit, writing leftovers to file!\n")
+				with open('./retries.txt','w', encoding="utf8") as retryDest:
+					for retry in errorArray:
+						retryDest.write(retry["citation"]+"\n\n")
+			self.logger.logToDisplay("Done!")
+			self.startButton.Enable()
 
 class CitationDownloader(wx.Frame):
-	#mailto name is so that cross ref likes us
-	filename = ""
+	#mailto name is so that cross ref likes us and lets use their special pool
+	fileName = ""
 	saveDir = ""
 	def __init__(self, *args,**kw):
 		super(CitationDownloader,self).__init__(*args,**kw,size=(700,600))
 		panel = wx.Panel(self)
-		#file picker, button, log
 		self.startButton = wx.Button(panel,label="Start download")
 		selectFileButton = wx.Button(panel, label="Select citation file")
 		clearButton = wx.Button(panel, label="Clear")
@@ -151,8 +90,8 @@ class CitationDownloader(wx.Frame):
 		with wx.FileDialog(self, "Select file", wildcard="TXT files (*.txt)|*.txt", style=wx.FD_OPEN|wx.FD_FILE_MUST_EXIST) as fileDialog:
 			if fileDialog.ShowModal() == wx.ID_CANCEL:
 				return     
-			self.filename = fileDialog.GetPaths()[0]
-			self.pathDisplay.SetLabel(self.filename)
+			self.fileName = fileDialog.GetPaths()[0]
+			self.pathDisplay.SetLabel(self.fileName)
 	
 	def dirSelectClicked(self, event):
 		with wx.DirDialog(self, "Select folder to save results to", style=wx.FD_OPEN | wx.FD_FILE_MUST_EXIST) as dirDialog:
@@ -163,17 +102,24 @@ class CitationDownloader(wx.Frame):
 			
 	def downloadClicked(self, event):
 		mailName = self.mailTo.GetValue()
-		if(self.filename != "" and self.saveDir != "" and mailName != ""):
-			self.startButton.Disable()
-			thread = PDFThread(self.filename, mailName, self.logDisplay, self.saveDir, self.startButton)
-			thread.start()
+		if(mailName != ""):
+			if(self.fileName != "" and self.saveDir != ""):
+				if not os.path.exists(self.saveDir):
+					os.makedirs(self.saveDir)
+				self.startButton.Disable()
+				logger = util.Logger(self.logDisplay, self.saveDir)
+				options = {"fileName": self.fileName, "mailName": mailName, "saveDir":self.saveDir}
+				thread = PDFThread(options, logger, self.startButton)
+				thread.start()
+		else:
+			self.logDisplay.AppendText("Please enter an email address - CrossRef uses a special pool for those who provide a mailTo\n\n")
 			
 	def clearClicked(self, event):
 		self.logDisplay.SetValue("")
 		self.saveDir = ""
 		self.dirDisplay.SetLabel(self.saveDir)
-		self.filename = ""
-		self.pathDisplay.SetLabel(self.filename)
+		self.fileName = ""
+		self.pathDisplay.SetLabel(self.fileName)
 			
 app = wx.App()
 frame = CitationDownloader(None, title="Citation Downloader")
---
+++ util.py
@@ -0,0 +1,74 @@
+import urllib.parse
+import requests
+from bs4 import BeautifulSoup
+
+class Logger:
+	def __init__(self, logDisplay, saveDir):
+		self.logDisplay = logDisplay
+		self.saveDir = saveDir
+		
+	def logToDisplay(self, text):
+		self.logDisplay.AppendText(f'{text}\n')
+		
+	def logMapping(self, text):
+		with open(f'{self.saveDir}/list.txt','w') as file:
+			file.write(f'{text}\n')
+			
+	def savePDF(self, name, data):
+		with open(f'{self.saveDir}/{name}.pdf','wb') as dest:
+			dest.write(data)
+
+class SciHubError(Exception):
+    def __init__(self, message, data):
+        super().__init__(message)
+        self.data = data
+			
+def getMetadata(citation, mailName):
+	data = requests.get("https://api.crossref.org/works?"+urllib.parse.urlencode({"query.bibliographic":citation,"mailto":mailName}))
+	response = data.json()["message"]['items'][0]
+	return (response["DOI"], response["title"][0])
+		
+def getPDF(citationRaw, logger, mailName):
+	citation = citationRaw.replace("\n"," ")
+	if(citation != ""):
+		logger.logToDisplay("Looking up metadata...")
+		doi, title = getMetadata(citation, mailName)
+		logger.logToDisplay(f'Found DOI {doi} for {title} from {citation}!')
+		getFromSciHub({"doi":doi,"title":title,"citation":citation},logger)
+	else:
+		logger.logToDisplay("Skipping blank citation entry\n")
+		
+def getFromSciHub(data, logger):
+	try:
+		pdfUrl = ""
+		logger.logToDisplay("Attempting to get PDF url...")
+		
+		articlePage = BeautifulSoup(requests.get("https://sci-hub.tw/"+data["doi"]).text,'html5lib')
+		iframes = articlePage.find_all("iframe")
+		if len(iframes) > 0:
+			pdfUrl = iframes[0]["src"]
+		else:
+			logger.logToDisplay("Hit PDF URL captcha, adding to retry list\n")
+			raise SciHubError('url captcha', data)
+				
+		logger.logToDisplay("PDF url found for: " + data["title"])
+		
+		#check if the response is a captcha by checking content type
+		response = requests.get(pdfUrl)
+		if(response.headers["Content-Type"] != "application/pdf"):
+			logger.logToDisplay("Hit PDF download captcha, adding to retry list\n")
+			raise SciHubError('download captcha', data)
+			
+		#in case there are funky characters
+		name = urllib.parse.quote(data["title"])
+		logger.savePDF(name, response.content)
+		
+		logger.logMapping(data["citation"]+"\t"+data["doi"]+"\t"+data["title"])
+		logger.logToDisplay("Downloaded PDF!\n")
+		
+	except Exception as e:
+		if(type(e) is SciHubError):
+			raise e
+		print(e)
+		logger.logToDisplay("Hit timeout, adding to retry list\n")
+		raise SciHubError('timeout', data)
